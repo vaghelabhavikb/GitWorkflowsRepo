bffbee3163674adbdda2b0d40d7c3158c0d8fd64
vaghelabhavikb <103305324+vaghelabhavikb@users.noreply.github.com>
2026-02-25 17:47:03 +0530

    Solved maxSpan problem (#33)
    Co-authored-by: vaghelabhavikb1 <106273070+vaghelabhavikb1@users.noreply.github.com>

--- codingbat.ts
+++ codingbat.ts
@@ -1,3 +1,30 @@
+let l: number[] = [1, 4, 2, 1, 4, 4, 4]
+let len = l.length
+let result = 0
+let sMap = new Map<number, number>()
+let eMap = new Map<number, number>()
+
+for (let i = 0; i < len; i++) {
+    if (!sMap.has(l[i])) {
+        sMap.set(l[i], i)
+    } else {
+        eMap.set(l[i], i)
+    }
+}
+
+    sMap.forEach((value: number, key: number) => {
+        if (eMap.has(key)) {
+            const eMapValue = eMap.get(key);
+            if (eMapValue !== undefined) {
+                result = Math.max(result, eMapValue - value + 1);
+            }
+        }
+        else {
+            result = Math.max(result, 1)
+        }
+    })
+
+    console.log(result)
 function canBalance() {
     let l = [2]
     let len = l.length
